a314570157bdcae1c8fca3b3db7cdba09a06ed18
Lauren Rouse <lrouse@Laurens-MacBook-Air.local>
2019-01-07 20:32:08 -0600

    Implemented Google Authentication

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,61 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import connection from '../helpers/data/connection';
+
+import Auth from './components/Auth/Auth';
+import MyNavBar from './components/MyNavbar/MyNavbar';
 import './App.scss';
+import authRequests from '../helpers/data/authRequests';
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    connection();
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+        });
+      } else {
+        this.setState({
+          authed: false,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
+    const { authed } = this.state;
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      this.setState({ authed: false });
+    };
+
+    if (!authed) {
+      return (
+        <div className="App">
+          <MyNavBar isAuthed={authed} logoutClickEvent={logoutClickEvent}/>
+          <Auth isAuthenticated={this.isAuthenticated} />
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <button className='btn btn-danger'>HELP ME</button>
-        <Button
-          tag="a"
-          color="success"
-          size="large"
-          href="http://reactstrap.github.io"
-          target="_blank"
-        >View Reactstrap Docs</Button>
+        <MyNavBar isAuthed={authed} logoutClickEvent={logoutClickEvent}/>
+        You are authenticated.
       </div>
     );
   }
